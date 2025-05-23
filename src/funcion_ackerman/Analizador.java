e05f4b61d7e10a0f03a931ece35e90539176fbd2
DavainCI <rodrigodavalos2105@gmail.com>
2025-05-23 00:52:24 -0500

    act

--- src/funcion_ackerman/Analizador.java
+++ src/funcion_ackerman/Analizador.java
@@ -1,15 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package funcion_ackerman;
 
 public class Analizador {
 
     public void mostrarEstadisticas(long inicio, long fin, int llamadas) {
         long tiempoTotal = fin - inicio;
-        System.out.println("Tiempo de ejecución: " + tiempoTotal + " nanosegundos");
-        System.out.println("Número de llamadas recursivas: " + llamadas);
+        System.out.println("Tiempo de ejecucion: " + tiempoTotal + " nanosegundos");
+        System.out.println("Numero de llamadas recursivas: " + llamadas);
     }
 
 }
